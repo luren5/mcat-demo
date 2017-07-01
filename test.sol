c6c97ccb77aa4d39cad181aa5010f78a9345200f
luren5 <email@luren5.com>
2017-07-01 23:50:01 +0800

    Init mcat demo project

---
+++ test.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.4;
+contract test { 
+    string public a;
+    function modify_a(string val) {
+        a = val;
+    }
+}
+
+contract test1 { 
+    string public a;
+    function modify_a(string val) {
+        a = val;
+    }
+}
+
+contract test2 { 
+    string public a;
+    function modify_a(string val) {
+        a = val;
+    }
+}
